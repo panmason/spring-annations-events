b5df3361655443ca9b557e9248b93946698334a3
mason <18643916953@163.com>
2023-09-23 21:46:25 +0800

    Create CustomBeanPostProcessor.java
    bean post processor add

---
+++ src/main/java/cn/com/cakelite/tartcaker/processor/CustomBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package cn.com.cakelite.tartcaker.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization:" +  beanName + "=======>" + bean.getClass());
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization:" + beanName + "=======>" + bean.getClass());
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
